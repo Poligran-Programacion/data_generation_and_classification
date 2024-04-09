105c3363d700f20ddea78de381775b8ddc3b4f16
Daniel Cubides <81591661+Dani1artu@users.noreply.github.com>
2024-04-09 13:27:51 -0500

    The CSV containing the seller info and total sale is added
    Completed code for the seller CSV, changes in the paths may be needed.

--- src/main/java/com/poligran/service/GenerateSalesReportFiles.java
+++ src/main/java/com/poligran/service/GenerateSalesReportFiles.java
@@ -1,8 +1,6 @@
 package com.poligran.service;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -15,109 +13,142 @@ import java.util.stream.Collectors;
 
 public class GenerateSalesReportFiles {
 	
-	public static int sellerReader (String args[]) {
-	String path = "C:\\Data\\Files\\Programing\\"
+	public static void individualSalesReader (String args[]) {
+		
+		/*
+		 * The path where the seller info file is specified.
+		 */
+		
+		String pathSeller = "C:\\Data\\Files\\Programing\\"
 			+ "data_generation_and_classification-main"
 			+ "\\src\\main\\resources\\base\\seller.txt";
-	String line = "";
-	int sellers = 0;
-	
-	HashMap<Integer, String> sellerNames = new HashMap<>();
-	
-	try {
-		BufferedReader br = new BufferedReader(new FileReader(path));
-		while((line = br.readLine()) != null) {
-			sellers += 1;
-			String[] values = line.split(";");
-			sellerNames.put(sellers,values[2]);
-			}
-		//System.out.println(sellerNames);//Tester
-		} catch (FileNotFoundException e){
-		e.printStackTrace();
-		} catch (IOException e) {
-		e.printStackTrace();
-		}
-	return sellers;
-	}
-	
-	public static void individualSalesReader (String args[], int sellers) {
-	String line = "";
-	int a = 0;
-	int sumPrices = 0;
-	HashMap<Integer, Integer> sellerSales = new HashMap<>();
-	
-	for (int i = 0; i<sellers; i++) {
+		String line1 = "";
 		
-		String path = "C:\\Data\\Files\\Programing\\"
+		int sellers = 0;//"sellers" variable is created for counting the number of sellers.
+		/*
+		 * The hashmaps where the info for Names,
+		 * Id types and Id's are created.
+		 */
+		
+		HashMap<Integer, String> sellerNames = new HashMap<>();
+		HashMap<Integer, String> sellerIds = new HashMap<>();
+		HashMap<Integer, String> sellerIdType = new HashMap<>();
+	
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(pathSeller));
+			while((line1 = br.readLine()) != null) {
+				sellers += 1;
+				String[] values = line1.split(";");
+				sellerNames.put(sellers,values[2]);
+				sellerIds.put(sellers,values[1]);
+				sellerIdType.put(sellers,values[0]);
+				}
+			//System.out.println(sellerNames);//Tester
+			//System.out.println(sellerIds);//Tester
+			//System.out.println(sellerIdType);//Tester
+			} catch (FileNotFoundException e){
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+		/*
+		 * A loop is used to read the file and write to each hashmap the key
+		 * (number corresponding the seller) and the value (Names,Id types,Id's).
+		 */
+		
+		String line2 = "";
+		int a = 0;//This variable is used to change the file name in order to account for "sellers" variable.
+		int sumPrices = 0;
+		HashMap<Integer, Integer> sellerSales = new HashMap<>();
+		
+		/*
+		 * "sellerSales" hashmap is created, it will store the key (number of seller)
+		 * and the value (total sales).
+		 */
+		
+		for (int i = 0; i<sellers; i++) {
+			
+			/*
+			 * The path where the seller sales file is specified,
+			 * it changes for each seller.
+			 */
+			
+			String pathSales = "C:\\Data\\Files\\Programing\\"
 				+ "data_generation_and_classification-main"
 				+ "\\src\\main\\resources\\sales"
 				+ "\\" + (1712618559+a) + "_Seller" + (1+a) + ".txt";
-		a+=1;
+			a+=1;
 			
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(path));
-			while((line = br.readLine()) != null) {
-				String[] values = line.split(";");
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(pathSales));
+				while((line2 = br.readLine()) != null) {
+					String[] values = line2.split(";");
 				
-				String prices = values[2];
-				String pricesc1 = prices.replace("$", "");
-				if (pricesc1.contains("-")) {
-					System.out.println("Error, check for negative prices");
-					System.exit(0);
+					String prices = values[2];
+					String pricesc1 = prices.replace("$", "");
+					if (pricesc1.contains("-")) {
+						System.out.println("Error, check for negative prices");
+						System.exit(0);
+					}
+					int pricesfinal = Integer.valueOf(pricesc1);
+					sumPrices = sumPrices + pricesfinal;
+					}
+				sellerSales.put(a,sumPrices);
+				sumPrices = 0;
+				} catch (FileNotFoundException e){
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-				int pricesfinal = Integer.valueOf(pricesc1);
-				sumPrices = sumPrices + pricesfinal;
-				}
-			sellerSales.put(a,sumPrices);
-			sumPrices = 0;
-			} catch (FileNotFoundException e){
-			e.printStackTrace();
-			} catch (IOException e) {
-			e.printStackTrace();
 			}
-		}
-	//System.out.println(sellerSales);//Tester
+		
+		/*
+		 * A loop is used to read the file and write to each hashmap.
+		 */
+		//System.out.println(sellerSales);//Tester
 
-	Map <Integer, Integer> sortedSellerSales = sellerSales.entrySet().stream()
-	.sorted((k1, k2) -> -k1.getValue().compareTo(k2.getValue()))
-	.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+		Map <Integer, Integer> sortedSellerSales = sellerSales.entrySet().stream()
+			.sorted((k1, k2) -> -k1.getValue().compareTo(k2.getValue()))
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
 			(e1, e2) -> e1, LinkedHashMap::new));
-	HashMap<String, String> printSellerSales = new HashMap<>();
-	sortedSellerSales.forEach((k, v) -> printSellerSales.put(k.toString(), v.toString()));
-	
-	//System.out.println(sortedSellerSales);//Tester
-	try {
-	      File myObj = new File("sellerSales.txt");
-	      if (myObj.createNewFile()) {
-	        System.out.println("File created: " + myObj.getName());
-	      } else {
-	        System.out.println("File already exists.");
-	      }
-	}catch (IOException e) {
-		System.out.println("An error occurred.");
-		e.printStackTrace();
-	    }
-	BufferedWriter bf = null;
-    try {
-    	bf = new BufferedWriter(new FileWriter("sellerSales.txt")); 
-    	  
-        for (Map.Entry<Integer, Integer> entry :
-        	sortedSellerSales.entrySet()) { 
-            bf.write(entry.getKey() + ","
-                     + entry.getValue()); 
-            bf.newLine(); 
-        } 
-        bf.flush(); 
-    } 
-    catch (IOException e) { 
-        e.printStackTrace(); 
-    } 
-    finally { 
-        try { 
-            bf.close(); 
-        } 
-        catch (Exception e) { 
-        } 
-    } 
-    }
+		
+		//"sellerSales" hashmap is sorted to "sortedSellerSales".
+		
+		HashMap<Integer, String> printSellerSales = new HashMap<>();
+		sellerSales.forEach((k, v) -> printSellerSales.put(k, v.toString()));
+		
+		/*
+		 * A second hashmap named "printSellerSales" is created, it contains the
+		 * same keys and values as "sellerSales" but changes the type of the value
+		 * from Integer to String to be able to write it later.
+		 */
+		
+		//System.out.println(sortedSellerSales);//Tester
+		//System.out.println(printSellerSales);//Tester
+		
+		try (FileWriter csvWriter = new FileWriter("Total_sales_by_seller.csv")) {
+			csvWriter.append("Name,Id Type,Id Number, Total Sales\n");// Write header row
+			for (Map.Entry<Integer, Integer> entry : sortedSellerSales.entrySet()) {
+				int key = entry.getKey();
+				
+				/*
+				 * Using a loop the hashmap sortedSellerSales is read and the values are stored to
+				 * "key" variable, in order to use it for reading the other hashmaps and write
+				 * the CSV file.
+				 */
+				
+				//System.out.println(key);//Tester
+				csvWriter.append(sellerNames.get(key)).append(",");
+				csvWriter.append(sellerIdType.get(key)).append(",");
+				csvWriter.append(sellerIds.get(key)).append(",");
+				csvWriter.append("$"+printSellerSales.get(key)).append("\n");
+			}
+			System.out.println("Total sales by seller file created");
+			csvWriter.close();
+		} catch (IOException e) {
+			System.out.println("Error creating report");
+			e.printStackTrace();
+		}
+	}	
 }		
